8adfdfe31742cb5cfc637f6b64dcb38c365602d4
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2020-06-18 17:28:49 -0300

    Otimizando

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 import streamlit as st
 import pages
 
-PAGES = {
-    "Início": pages.InicioPage(),
-    "Biblioteca": pages.BibliotecaPage(),
-    #"Cursos": pages.CursoPage(),
-    "Professores": pages.ProfessorPage(),
-}
+
+@st.cache
+def get_page():
+    print('CHAMOU get_page')
+    return {
+        "Início": pages.InicioPage(),
+        "Biblioteca": pages.BibliotecaPage(),
+        # "Cursos": pages.CursoPage(),
+        "Professores": pages.ProfessorPage(),
+    }
 
 
 def main():
     st.sidebar.title("Menu de Navegação")
-    selection = st.sidebar.radio("Ir para", list(PAGES.keys()))
-    page = PAGES[selection]
+    selection = st.sidebar.radio("Ir para", list(get_page().keys()))
+    page = get_page()[selection]
 
     with st.spinner(f"Carregando {selection} ..."):
         page.render()
+
+    print('----------')
+    print('app')
 
 
 if __name__ == "__main__":

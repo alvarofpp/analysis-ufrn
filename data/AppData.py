a29e65cf8d0f76f38003f15445a4f73d22893266
alvarofpp <alvarofepipa@gmail.com>
2020-11-29 19:31:09 -0300

    chore: app data in cache

--- data/AppData.py
+++ data/AppData.py
@@ -1,14 +1,28 @@
 import json
 from typing import Dict
+import streamlit as st
 
 
 class AppData:
 
     @staticmethod
     def save(data: Dict) -> None:
-        with open('app_data.json', 'w') as outfile:
+        with open('data/app_data.json', 'w') as outfile:
             json.dump(data, outfile, indent=2, sort_keys=True)
 
     @staticmethod
-    def get_data_by_key(key: str) -> Dict:
-        return json.load(open('app_data.json'))[key]
+    def data_get(key: str) -> Dict:
+        data = AppData.__data()
+
+        if '.' in key:
+            keys = key.split('.')
+
+            for k in keys:
+                data = data[k]
+
+        return data
+
+    @staticmethod
+    @st.cache
+    def __data() -> Dict:
+        return json.load(open('data/app_data.json'))

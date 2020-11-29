d0112ddf3b183ec6b08f8ca1ce79533c22c24476
alvarofpp <alvarofepipa@gmail.com>
2020-11-29 17:36:34 -0300

    fix: render's cache

--- pages/PageView.py
+++ pages/PageView.py
@@ -7,12 +7,11 @@ class PageView(abc.ABC):
     def __init__(self):
         self.title = ''
 
-    @st.cache(suppress_st_warning=True)
     def render(self):
         st.title(self.title.upper())
         self.horizontal_rule()
         self.template()
-        for section in self.get_sections():
+        for section in self.__get_sections():
             getattr(self, section)()
 
     @abc.abstractmethod
@@ -22,5 +21,5 @@ class PageView(abc.ABC):
     def horizontal_rule(self):
         st.markdown('----------')
 
-    def get_sections(self):
+    def __get_sections(self):
         return sorted([dir for dir in self.__dir__() if dir.startswith('section_')])

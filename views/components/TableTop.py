d2203f5e7a69cc0625ec7b852bfc149d5fe3c36f
alvarofpp <alvarofepipa@gmail.com>
2020-12-01 15:55:28 -0300

    refactor: TableTop component

--- views/components/TableTop.py
+++ views/components/TableTop.py
@@ -5,7 +5,7 @@ class TableTop:
 
     @staticmethod
     def render(data, column: str, top: int):
-        table_qet = data.sort_values(column, ascending=False)[:top].reset_index()
-        table_qet.index = table_qet.index + 1
-        table_qet.drop(columns=['index'], inplace=True)
-        st.table(table_qet)
+        table_data = data.sort_values(column, ascending=False)[:top].reset_index()
+        table_data.index = table_data.index + 1
+        table_data.drop(columns=['index'], inplace=True)
+        st.table(table_data)
